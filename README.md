f2752f25a31c4e7a814094fe66140d03358ebc73
Ray <pacmanguris@gmail.com>
2025-03-21 23:59:59 +0100

    WE FIXED EVERYTHING!

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Luxury Industries is small vanilla like mod which adds different small but useful futures into the game.
 
-_Created by Team Evolution: Ray, Alpha_Marinade and Magafinz_
+_Created by Team Evolution: ray_s, Alpha_Marinade and Magafin_
--- common/src/main/java/com/TeamEvo/luxuryIndustries/LuxuryIndustries.java
+++ common/src/main/java/com/TeamEvo/luxuryIndustries/LuxuryIndustries.java
@@ -1,7 +1,7 @@
 package com.TeamEvo.luxuryIndustries;
 
 public final class LuxuryIndustries {
-    public static final String MOD_ID = "luxury-industries";
+    public static final String MOD_ID = "luxury_industries";
 
     public static void init() {
         // Write common init code here.
